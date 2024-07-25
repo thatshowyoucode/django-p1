68efa14778984f142e011c41e9db92cfa7f2e735
thatshowyoucode <sameermardani@gmail.com>
2024-07-25 13:44:35 +0530

    message

--- Hello/settings.py
+++ Hello/settings.py
@@ -121,3 +121,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Added manually
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+    
+]
